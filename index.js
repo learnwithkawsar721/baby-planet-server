cff0ee219336fa8d4c0495c93837596dcbfcf19c
learnwithkawsar <kawsar721g@gmail.com>
2021-11-11 10:17:17 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.858ok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("baby_planet");
+    const userCollection = database.collection("users");
+    // User Insert
+    app.post("/users", async (req, res) => {
+      const result = await userCollection.insertOne(req.body);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
